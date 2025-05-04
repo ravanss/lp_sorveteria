c3a008c48fa902f28ef47e4a4ae544a6337789c8
Saulo Pedro da Silva <psaulo7@gmail.com>
2025-05-04 16:02:32 -0300

    subindo melhoria na home

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri()?>/style.css">
 </head>
 <body>
-    <header id="header" class="container">
+    <header id="header" class="container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -140,13 +140,15 @@
     <footer>
         <div class="container">
             <div class="row">
-                <div class="col-lg-7 col-md-12 ">
+                <div class="col-lg-7 col-md-12 d-flex">
                     <div class="footer-content">
                         <h2 class="footer-title">Faça o seu pedido por:</h2>
                         <a href="#">(11)99999-9999</a>
                         <p>Rua dos Sorvetes, 123 - São Paulo - SP</p>
                         <p>Segunda a Sexta: 10h às 22h</p>
                         <p>Sábado e Domingo: 10h às 23h</p>
+                    </div>
+                    <div class="footer-social-media">
                         <h3>FIQUE POR DENTRO DE TUDO NaS SIGA NAS REDES SOCIAIS</h2>
                         <ul class="social-media">
                             <li>
@@ -194,6 +196,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/js/all.min.js"></script>
-    <script src="<?php echo get_stylesheet_directory_uri()?>/main.js"></script>
+    <script src="<?php echo get_template_directory_uri()?>/assets/js/main.js"></script>
 </body>
 </html>
